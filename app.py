b52b19737a12a294c1306fc1943d9ad543fd5df3
Temitayo Charles Akinniranye <s9charles.wft@gmail.com>
2026-03-21 01:44:48 -0400

    updated application code

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from python3 import datetime
-from flask import Flask 
+from datetime import datetime
+from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return '__This is a simple Flask app and it tells my local time__'  + str(datetime.now())   
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)  
-    
+    return "__This is a simple Flask app and it tells my local time__ " + str(datetime.now())
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
