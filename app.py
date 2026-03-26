6f72277ccae3e14d5ed77a78a710cdd4196916c4
Temitayo <temitayo_charles@yahoo.com>
2026-03-26 15:50:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "__This is a simple Flask app and it tells my local time__ " + str(datetime.now())
+    return "__This is a super simple Flask app and it tells my local time__ " + str(datetime.now())
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
